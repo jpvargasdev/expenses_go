fe71551dc1822a0858d1b337b21d7bce0f786cb1
Juan Vargas <vargasm.jp@gmail.com>
2024-12-25 18:15:27 +0100

    fix wsgi port

--- wsgi.py
+++ wsgi.py
@@ -11,14 +11,13 @@ sys.path.insert(0, os.path.realpath(os.path.join(cur_dir, '../sqlite_web')))
 
 from sqlite_web import sqlite_web
 
-port = os.getenv("PORT", "8081")
 password = os.getenv("SQLITE_WEB_UI_PASSWORD")
 
 def main(db_file):
     sqlite_web.initialize_app(db_file, password=password)
 
     pool = Pool(50)
-    server = WSGIServer(('0.0.0.0', int(port)), sqlite_web.app, log=None, spawn=pool)
+    server = WSGIServer(('0.0.0.0', int("8081")), sqlite_web.app, log=None, spawn=pool)
     server.serve_forever()
 
 
