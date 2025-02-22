11a346e4ad05dbbba64839830ad96a8ac5c13d95
Juan Vargas <vargasm.jp@gmail.com>
2025-02-22 22:19:36 +0100

    fix: decode from base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ if [ -z "$GOOGLE_APPLICATION_CREDENTIALS_JSON" ]; then
   exit 1
 fi
 
-# Write the JSON string to a file
-echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" | jq '.' > /app/firebase-config.json
+# Decode the Base64 string and write it to a file
+echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" | base64 -d > /app/firebase-config.json
 
 # Export the path to make Firebase SDK recognize it
 export GOOGLE_APPLICATION_CREDENTIALS="/app/firebase-config.json"
 
-# Start the Go application and wsgi in the background
+# Start the Go application
 ./guilliman  # Start the Go app
 
 wait
